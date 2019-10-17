8167f96ad94653605db5e650f124ae4d2c2d869d
Coande <e12e@qq.com>
2019-10-17 16:36:03 +0800

    first commit

---
+++ src/test/java/com/e12e/cloudeurekaservice/CloudEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e12e.cloudeurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
